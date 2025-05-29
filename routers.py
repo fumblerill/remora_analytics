32b4046ae09bdc0571508255c298a0aab91374ae
fumblerill <shidman01@yandex.ru>
2025-05-29 16:36:49 +1000

    Добавил сохранение xlsx таблицы

--- routers.py
+++ routers.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Request, UploadFile, File, Form
 from fastapi.responses import HTMLResponse, RedirectResponse
 from xlsx_handler import generate_tables
+import os
 
 from settings import USERS
 
@@ -16,6 +17,37 @@ async def index(request: Request):
     user = request.session.get("user")
     if not user:
         return RedirectResponse(url="/login", status_code=302)
+
+    if os.path.exists("uploaded/latest.xlsx"):
+        with open("uploaded/latest.xlsx", "rb") as f:
+            contents = f.read()
+        data = generate_tables(contents)
+
+        return request.app.templates.TemplateResponse("upload.html", {
+        "request": request,
+        "filename": "latest.xlsx",
+
+        # JSON-данные для Tabulator
+        "raw_data": data["raw_data"],
+        "statistics_data": data["statistics_data"],
+        "doc_perf_data": data["doc_perf_data"],
+        "types_data": data["types_data"],
+        "cert_errors_data": data["cert_errors_data"],
+        "reference_data": data["reference_data"],
+
+        # Данные для диаграмм
+        "pie_labels": data["pie_labels"],
+        "pie_values": data["pie_values"],
+        "bar_labels": data["bar_labels"],
+        "bar_values": data["bar_values"],
+
+        # Сводка и даты
+        "summary": data["summary"],
+        "min_date": data["min_date"],
+        "max_date": data["max_date"],
+
+        "theme": request.cookies.get("theme", "light")
+    }) 
 
     theme = request.cookies.get("theme", "light")
     return request.app.templates.TemplateResponse("upload.html", {
@@ -65,6 +97,11 @@ async def upload(request: Request, file: UploadFile = File(...)):
         })
 
     contents = await file.read()
+
+    os.makedirs("uploaded", exist_ok=True)
+    with open("uploaded/latest.xlsx", "wb") as f:
+        f.write(contents)
+
     data = generate_tables(contents)
     theme = request.cookies.get("theme", "light")
 
